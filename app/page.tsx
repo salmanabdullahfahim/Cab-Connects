0e0bb1346e3c959e38aee3dcce0e5177cd8667ee
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-04 12:13:11 +0600

    layout created for booking and map section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <h2>Hello hello everyone</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="bg-teal-200">Booking</div>
+        <div className="col-span-2 bg-red-200">Map</div>
+      </div>
     </div>
   );
 }
